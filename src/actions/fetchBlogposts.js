e1a2421894c5a28548d487580cd9ec1c0fbb4929
MDAM182 <mdam2345@gmail.com>
2021-07-03 14:57:20 -0400

    fetch request for all blog post

--- src/actions/fetchBlogposts.js
+++ src/actions/fetchBlogposts.js
@@ -1,3 +1,12 @@
 export function fetchBlogposts(){
+  return (dispatch) => {
+    fetch('http://localhost:3000/api/v1/blog_posts')
+        .then(resp => resp.json())
+        .then(blog_posts => dispatch({
+            type: 'FETCH_BLOGPOSTS',
+            payload: blog_posts
 
+        }))
+        .catch(error => console.log(error) )
+}
 }
