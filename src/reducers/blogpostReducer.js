00469a6f672967cac170c1507fa272f2e8140da4
MDAM182 <mdam2345@gmail.com>
2021-07-03 14:58:42 -0400

    reducers created for action methods

--- src/reducers/blogpostReducer.js
+++ src/reducers/blogpostReducer.js
@@ -1,4 +1,31 @@
 export default function blogpostReducer(state = { blog_posts: [] }, action) {
 
-    return state
+    switch (action.type) {
+        case 'FETCH_BLOGPOSTS':
+
+            return { blog_posts: action.payload }
+
+        case 'ADD_BLOGPOST':
+            return { ...state, blog_posts: [...state.blog_posts, action.payload] }
+
+        case 'EDIT_BLOGPOST':
+
+            let blog_postEdit = state.blog_posts.map(blog_post => {
+
+                if (blog_post.id === action.payload.id) {
+                    return action.payload
+                } else {
+                    return blog_post
+                }
+            })
+
+            return { ...state, blog_posts: blog_postEdit }
+
+
+
+        default:
+            return state
+
+
+     }
  }
